4ab8c76e1e5dc423294cb9d1614c1778b824a580
pedroagaponto <pedrohmpereira@gmail.com>
2018-06-20 08:45:14 -0300

    Update to build target to arm

--- app/jni/Application.mk
+++ app/jni/Application.mk
@@ -5,7 +5,8 @@ APP_STL := c++_static
 APP_CPPFLAGS += -w -frtti
 
 #APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_ABI := x86
+APP_ABI := armeabi-v7a x86
+#APP_ABI := x86
 
 # Min runtime API level
 APP_PLATFORM=android-14
---
+++ app/jni/src/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+SDL_PATH := ../SDL
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+
+# Add your application source files here...
+LOCAL_SRC_FILES :=  Alan.cpp AlanAnimation.cpp BigAlan.cpp Camera.cpp Common.cpp Component.cpp Game.cpp GameObject.cpp GridControl.cpp InputManager.cpp Interpol.cpp Light.cpp Main.cpp MiniTileMap.cpp Music.cpp Parallax.cpp Rect.cpp Resources.cpp Sound.cpp Sprite.cpp StageState.cpp State.cpp Text.cpp TileMap.cpp TileSet.cpp Timer.cpp TitleState.cpp Vec2.cpp
+
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -Wl,--no-warn-shared-textrel
+
+LOCAL_DISABLE_FATAL_LINKER_WARNINGS := true
+
+include $(BUILD_SHARED_LIBRARY)
