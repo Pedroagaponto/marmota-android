892f7151c7e1f003fd0f999c1d6db79cce557fa4
pedroagaponto <pedrohmpereira@gmail.com>
2018-07-08 06:16:34 -0300

    update

--- app/jni/src/Android.mk
+++ app/jni/src/Android.mk
@@ -9,7 +9,7 @@ SDL_PATH := ../SDL
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
 # Add your application source files here...
-LOCAL_SRC_FILES :=  Alan.cpp AlanAnimation.cpp BigAlan.cpp Camera.cpp Common.cpp Component.cpp Game.cpp GameObject.cpp GridControl.cpp InputManager.cpp Interpol.cpp Light.cpp Main.cpp MiniTileMap.cpp Music.cpp Parallax.cpp Rect.cpp Resources.cpp Sound.cpp Sprite.cpp StageState.cpp State.cpp Text.cpp TileMap.cpp TileSet.cpp Timer.cpp TitleState.cpp Vec2.cpp
+LOCAL_SRC_FILES := Alan.cpp AlanActionControl.cpp AlanAnimation.cpp AlanItemCount.cpp BigAlan.cpp Camera.cpp Common.cpp Component.cpp Enemy.cpp EnemySpawn.cpp Game.cpp GameObject.cpp GridControl.cpp HudCounter.cpp HudMeter.cpp HudTimer.cpp InputManager.cpp Interpol.cpp Light.cpp Main.cpp MiniTileMap.cpp Music.cpp Parallax.cpp Rect.cpp Resources.cpp Sound.cpp Sprite.cpp StageState.cpp State.cpp TileMap.cpp TileSet.cpp Timer.cpp TitleState.cpp Vec2.cpp
 
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_ttf
 
